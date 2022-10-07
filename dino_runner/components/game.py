1fb7be4a38840a8f390ebf44e66612598ef2dc59
JosueSirpa <Josue.Sirpa@fundacion-jala.org>
2022-10-06 22:32:01 -0400

    grupo3-mod2 menu areglado con contador areglado no repite

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -3,6 +3,8 @@ from dino_runner.utils.constants import BG,ICON, RUNNING,SCREEN_HEIGHT,SCREEN_WI
 from dino_runner.components.dinosaur.dinosaur import Dinosaur
 from dino_runner.components.obtacles.obstaclemanager import ObstacleManager
 from dino_runner.components.menu_score.text_utils import *
+from dino_runner.components.player_hearts.PlayerHeartManager import PlayerHeartsManager
+from dino_runner.components.powerups.powerupmanager import PowerUpManager
 
 class Game:
     def __init__(self):
@@ -11,38 +13,43 @@ class Game:
         pygame.display.set_icon(ICON)
         self.screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT)) 
         self.clock = pygame.time.Clock()
-
         self.playing = False
         self.game_speed=20
         self.x_pos_bg = 0
         self.y_pos_bg = 380
-
-
-
         self.player = Dinosaur()
         self.obstacle_manager = ObstacleManager()
 
         self.points = 0
         self.running = True
 
+
+        self.death_count =0
+        self.player_heart_manager = PlayerHeartsManager()
+        self.power_up_manager = PowerUpManager()
+
     def run(self):
+        self.obstacle_manager.reset_obstacles(self)
+        self.player_heart_manager.reset_hearts()
+        self.power_up_manager.reset_power_ups(self.points)
         self.playing = True
         while self.playing:
             self.events()
             self.updates()
             self.draw()
 
-        pygame.quit()
     
     def events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.playing=False
+                self.running=False
 
     def updates(self):
         user_input=pygame.key.get_pressed()
         self.player.update(user_input)
         self.obstacle_manager.update(self)
+        self.power_up_manager.update(self.points,self.game_speed,self.player)
 
     def draw(self):
         self.clock.tick(FPS)
@@ -50,8 +57,9 @@ class Game:
         self.draw_background()
         self.player.draw(self.screen)
         self.obstacle_manager.draw(self.screen)
-
-        self.score()
+        self.score()       
+        self.player_heart_manager.draw(self.screen)
+        self.power_up_manager.draw(self.screen)
         pygame.display.update()
         pygame.display.flip()
 
@@ -66,19 +74,22 @@ class Game:
 
     def score(self):
         self.points = self.points + 1
+        # hacemos que el juego aumente de velocidad
         if self.points % 100 == 0:
             self.game_speed = self.game_speed + 1
-
+        
+        # imprimimos el score
         score, score_rect = get_score_element(self.points)
-
         self.screen.blit(score,score_rect)
+        self.player.check_invincibility(self.screen)
 
-    def show_menu(self,death_count = 0):
+
+    def show_menu(self):
         self.running = True
         white_color =(255,255,255)
         self.screen.fill(white_color)
 
-        self.print_menu_elements(death_count)
+        self.print_menu_elements(self.death_count)
 
         pygame.display.update()
 
@@ -112,6 +123,7 @@ class Game:
                 exit()
 
             if event.type == pygame.KEYDOWN:
+                self.__init__()
                 self.run()
 
                 
